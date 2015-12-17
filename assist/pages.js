e2baa7f88fe3b4ba3e4cdc537eec58ec3a9edfcd
Vladimir37 <vladimir_zapas@mail.ru>
2015-12-17 23:19:46 +0000

    Content 0.1

--- assist/pages.js
+++ assist/pages.js
@@ -2,11 +2,25 @@ function render_pages(req, res, next) {
     var lang = req.params.lang;
     var article = req.params.article;
     if(lang && article) {
-        res.render('main/' + lang + '/' + article);
+        res.render('main/' + lang + '/' + article, {}, function(err, result) {
+            if(err) {
+                res.render('errors/e404');
+            }
+            else {
+                res.end(result);
+            }
+        });
     }
     else {
         var index_lang = lang || article;
-        res.render('main/' + index_lang + '/index');
+        res.render('main/' + index_lang + '/index', {}, function(err, result) {
+            if(err) {
+                res.render('errors/e404');
+            }
+            else {
+                res.end(result);
+            }
+        });
     }
 };
 
